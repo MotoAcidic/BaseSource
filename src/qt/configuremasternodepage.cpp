0b1b6d70ca5898e7560000805a608e44a51f9004
TFinch <travisfinch01@gmail.com>
2021-06-25 19:11:36 -0400

    test

--- src/qt/configuremasternodepage.cpp
+++ src/qt/configuremasternodepage.cpp
@@ -401,9 +401,6 @@ void ConfigureMasternodePage::on_CreateTier1_clicked()
 
     // When done adding all the masternodes to the config close the file
     fclose(configFile);
-    clear();
-    std::string strErr;
-    read(strErr);
 
 }
     /*
